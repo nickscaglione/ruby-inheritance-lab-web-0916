053a4e452a003a2c0923966684f23d329666402f
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-10-02 11:26:41 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+
+    @knowledge = []
+  end
+
+  def learn(info)
+    knowledge << info
+  end
 
 end
